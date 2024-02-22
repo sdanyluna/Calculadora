d9eb162de200f4e6e7145494675eeb1eb2b9e61b
Ricklix <160774065+Ricklix@users.noreply.github.com>
2024-02-22 13:29:59 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,30 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-  
-        int suma,a,b;
-        int variable1, variable2, resultado;
-        printf("\t\tPrograma de la Calculadora\n");
-        printf("\tSUMA\n");
-        printf("Inserta el primer valor a sumar: ");
-        scanf("%i", a);
-        printf("Inserta el segundo valor a sumar: ");
-        scanf("%i", b);
-        suma=a+b;
-        printf("La suma de los dos numeros es: %i", suma);
-        printf("\n\n");
-       
-      
-        printf("\tMULTIPLICACION\n");
-        printf("Ingrese el primer valor a multiplicar");
-        scanf("%i", variable1);
-        printf("Ingrese el segundo valor a multiplicar");
-        scanf("%i", variable2);
-        resultado= variable1*variable2;
-        printf("El resultado a Multiplicar es: %i", resultado);
-       
-  
-  return 0;
-}
-//Pongan sus nombres a lo que hicieron
+int div1, div2, result;
+printf("\tDivision\n");
+  printf("Ingrese el primer valor a dividir");
+  scanf("%i", div1);
+  printf("Ingrese el segundo valor a dividir");
+  scanf("%i", div2);
+  result= div1/div2;
+  printf("El resultado a Multiplicar es: %i", result);
