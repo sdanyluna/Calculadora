7e75e9c4d70c01da5d7a22930841c4e4ed179610
sdanyluna <127701339+sdanyluna@users.noreply.github.com>
2024-02-22 13:22:52 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -2,19 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-  int opc = 1;
-
-  do {
-    printf("Introduce una de las siguientes opciones:\n");
-    printf("\n\t1\tSuma");
-    printf("\n\t2\tResta");
-    printf("\n\t3\tMultiplicacion");
-    printf("\n\t4\tDivision");
-    printf("\n\t5\tSalir\n");
-    scanf("%i",&opc);
-
-    switch (opc) {
-      case 1:
+  
         int suma,a,b;
         int variable1, variable2, resultado;
         printf("\t\tPrograma de la Calculadora\n");
@@ -26,9 +14,8 @@ int main() {
         suma=a+b;
         printf("La suma de los dos numeros es: %i", suma);
         printf("\n\n");
-        break;
-      case 2:
-      case 3:
+       
+      
         printf("\tMULTIPLICACION\n");
         printf("Ingrese el primer valor a multiplicar");
         scanf("%i", variable1);
@@ -36,14 +23,7 @@ int main() {
         scanf("%i", variable2);
         resultado= variable1*variable2;
         printf("El resultado a Multiplicar es: %i", resultado);
-        break; //Miranda Vargas Ricardo
-      case 4:
-      case 5:
-        return 0;
-        break;
-    }
-    
-  } while (opc != 2);
+       
   
   return 0;
 }
