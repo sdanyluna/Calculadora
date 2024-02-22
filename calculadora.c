9e8b2bcf834f238a7921c943a1edc3ed4cd6d8ac
Ricklix <160774065+Ricklix@users.noreply.github.com>
2024-02-22 12:52:32 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -11,5 +11,15 @@ int suma,a,b;
   
   cout<<"la suma de los dos numeros es:"<<suma;
 
+int variable1, variable2, resultado;
+printf("Ingrese el primer valor a multiplicar");
+scanf("%i", variable1);
+printf("Ingrese el segundo valor a multiplicar");
+scanf("%i", variable2);
+resultado= variable1*variable2;
+printf("El resultado a Multiplicar es: %i", resultado);
+
+  
   return 0;
 }
+
