f40ba0b801271af94a3178628698fbbe54b2b39f
Davidayala10 <160775748+Davidayala10@users.noreply.github.com>
2024-02-22 12:41:19 -0600

    Update calculadora.c
    suma

--- calculadora.c
+++ calculadora.c
@@ -1,1 +1,15 @@
+#includ <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+int suma,a,b;
+  cout<<"inserta los numeros a sumar";
+  cin>>a,b;
+  
+  suma=a+b;
+  
+  cout<<"la suma de los dos numeros es:"<<suma;
+
+  return 0;
+}
