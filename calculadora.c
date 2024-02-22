ab4b8535cadda2829f1f88f335dc86767fcf013f
JorgeMurilloFarfan <160550901+JorgeMurilloFarfan@users.noreply.github.com>
2024-02-22 13:16:55 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-int suma,a,b;
-int variable1, variable2, resultado;
+  int suma,a,b,opc;
+  int variable1, variable2, resultado;
   printf("\t\tPrograma de la Calculadora\n");
+  printf("Menu\n");
+  printf("Suma ..................... 1\n");
+  printf("Resta .................... 2\n");
+  printf("Multiplicacion ........... 3\n");
+  printf("Division ................. 4\n");
+  printf("Eliga una opcion: ");
+  scanf("%i", opc);
+  switch()
   printf("\tSUMA\n");
   printf("Inserta el primer valor a sumar: ");
   scanf("%i", a);
