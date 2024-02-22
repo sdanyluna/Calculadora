cafe047c3b0c61b9e887ddc6a93ba08bae286628
sdanyluna <127701339+sdanyluna@users.noreply.github.com>
2024-02-22 13:29:22 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -4,15 +4,6 @@
 int main() {
 int suma,a,b;
 int variable1, variable2, resultado;
-  printf("\t\tPrograma de la Calculadora\n");
-  printf("\tSUMA\n");
-  printf("Inserta el primer valor a sumar: ");
-  scanf("%i", a);
-  printf("Inserta el segundo valor a sumar: ");
-  scanf("%i", b);
-  suma=a+b;
-  printf("La suma de los dos numeros es: %i", suma);
-  printf("\n\n");
 
   printf("\tMULTIPLICACION\n");
   printf("Ingrese el primer valor a multiplicar");
