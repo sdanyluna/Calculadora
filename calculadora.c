668d6da4bfdbb00cf39a2bb6a18215b06c75810f
Ricklix <160774065+Ricklix@users.noreply.github.com>
2024-02-22 12:58:46 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,25 +1,26 @@
-#includ <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
-
-int main(){
+int main() {
 int suma,a,b;
-  cout<<"inserta los numeros a sumar";
-  cin>>a,b;
-  
-  suma=a+b;
-  
-  cout<<"la suma de los dos numeros es:"<<suma;
-
 int variable1, variable2, resultado;
-printf("Ingrese el primer valor a multiplicar");
-scanf("%i", variable1);
-printf("Ingrese el segundo valor a multiplicar");
-scanf("%i", variable2);
-resultado= variable1*variable2;
-printf("El resultado a Multiplicar es: %i", resultado);
+  printf("\t\tPrograma de la Calculadora\n");
+  printf("\tSUMA\n");
+  printf("Inserta el primer valor a sumar: ");
+  scanf("%i", a);
+  printf("Inserta el segundo valor a sumar: ");
+  scanf("%i", b);
+  suma=a+b;
+  printf("La suma de los dos numeros es: %i", suma);
+  printf("\n\n");
 
-  
+  printf("\tMULTIPLICACION\n");
+  printf("Ingrese el primer valor a multiplicar");
+  scanf("%i", variable1);
+  printf("Ingrese el segundo valor a multiplicar");
+  scanf("%i", variable2);
+  resultado= variable1*variable2;
+  printf("El resultado a Multiplicar es: %i", resultado);
   return 0;
 }
 
